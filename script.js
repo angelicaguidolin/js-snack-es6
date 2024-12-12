169b74fa02ba51021dbcf085d54bd0956ea901d8
angelica <guidolinangelica66@gmail.com>
2024-12-12 18:38:12 +0100

    feat add random number

--- script.js
+++ script.js
@@ -40,3 +40,32 @@ for(let i=0; i < bike.length;i++){
 }
 console.log(bikelight)
 
+
+
+/*Creare un array di oggetti di squadre di calcio.
+ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+const team=[
+    {teamName: "Milan", score: 0, soccerFouls: 0,},
+    {teamName: "Juventus", score: 0, soccerFouls: 0,},
+    {teamName: "Inter", score: 0, soccerFouls: 0,},
+    {teamName: "Venezia FC", score: 0, soccerFouls: 0,},
+]
+team["score"]= Math.random()*10
+//console.log(team[0])
+for(let teamScore of team){
+    if(teamScore.score ===0){
+        teamScore.score = Math.floor(Math.random()*10) 
+    }
+}
+
+console.log(team)
+for(let teamFouls of team){
+    if(teamFouls.soccerFouls===0){
+        teamFouls.soccerFouls=Math.floor(Math.random()*10) 
+    }
+}
+console.log(team)
